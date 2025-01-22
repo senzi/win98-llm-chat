60b0e2df80f56df94ea835d81c15b7ab8333dc06
阿木 <sennes111762@gmail.com>
2025-01-22 13:58:28 +0800

    🔧 chore: Update version number to 1.0.1 in App.vue
    Updated the version number in the about section of the App.vue file from v1.0.0 to v1.0.1.

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
         <span>作者：Windsurf & sennzi</span>
       </div>
       <div class="about-section" style="margin-left: auto;">
-        <span>v1.0.0</span>
+        <span>v1.0.1</span>
       </div>
     </div>
   </div>
